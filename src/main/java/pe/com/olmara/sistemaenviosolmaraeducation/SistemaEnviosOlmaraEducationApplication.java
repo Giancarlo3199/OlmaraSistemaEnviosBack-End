da39dce32833b0ce0933c8db453e0826ede1ba89
giancarlo <106542438+cheto3199@users.noreply.github.com>
2025-11-24 13:19:42 -0500

    Creacion Proyecto Sprint Boot
    Creacion del proyecto (Estructura Inicial):
    Lenguaje: Java
    Type: Maven
    JDK: 21
    Java: 17
    Packaging: Jar

---
+++ src/main/java/pe/com/olmara/sistemaenviosolmaraeducation/SistemaEnviosOlmaraEducationApplication.java
@@ -0,0 +1,13 @@
+package pe.com.olmara.sistemaenviosolmaraeducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEnviosOlmaraEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEnviosOlmaraEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/olmara/sistemaenviosolmaraeducation/SistemaEnviosOlmaraEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.olmara.sistemaenviosolmaraeducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEnviosOlmaraEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
